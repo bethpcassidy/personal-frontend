3b0c968e7769d173dafc06a9ade72779c85cce0d
beth <bethcassdy@gmail.com>
2023-04-11 13:29:39 -0400

    bootstrap cards

--- src/PostsIndex.jsx
+++ src/PostsIndex.jsx
@@ -1,12 +1,17 @@
 export function PostsIndex(props) {
   return (
-    <div>
-      <h1>All Posts</h1>
+    <div className="card text-center">
+      <h1 className="text-center">All Posts</h1>
       {props.posts.map((post) => (
         <div key={post.id}>
-          <h2>{post.title}</h2>
-          <h2>{post.subline}</h2>
-          <button onClick={() => props.onShowPost(post)}>More info</button>
+          <div className="card-header">
+            <h2>{post.title}</h2>
+          </div>
+          <div className="card-body">
+            <h5 class="card-title">{post.subline}</h5>
+            <p class="card-text">{post.body}</p>
+            <button onClick={() => props.onShowPost(post)}>More info</button>
+          </div>
         </div>
       ))}
     </div>
