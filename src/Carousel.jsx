fca5df21a471c85a91cc9a6becc63d2f932afc2e
beth <bethcassdy@gmail.com>
2023-04-24 17:31:47 -0400

    carousel updates

--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -2,6 +2,7 @@ import "./Carousel.css";
 import { CCarousel } from "@coreui/react";
 import { CCarouselItem } from "@coreui/react";
 import { CImage } from "@coreui/react";
+import { CCarouselCaption } from "@coreui/react";
 
 export function Carousel(props) {
   return (
@@ -13,6 +14,10 @@ export function Carousel(props) {
             src={"https://i.ytimg.com/vi/0xqkksHov58/maxresdefault.jpg"}
             alt="slide 1"
           />
+          <CCarouselCaption className="d-none d-md-block">
+            <h5>First slide label</h5>
+            <p>Some representative placeholder content for the first slide.</p>
+          </CCarouselCaption>
         </CCarouselItem>
         <CCarouselItem>
           <CImage
@@ -20,14 +25,21 @@ export function Carousel(props) {
             src={"https://i.ytimg.com/vi/0xqkksHov58/maxresdefault.jpg"}
             alt="slide 2"
           />
+          <CCarouselCaption className="d-none d-md-block">
+            <h5>Second slide label</h5>
+            <p>Some representative placeholder content for the first slide.</p>
+          </CCarouselCaption>
         </CCarouselItem>
         <CCarouselItem>
-          xs
           <CImage
             className="d-block w-100"
             src={"https://i.ytimg.com/vi/0xqkksHov58/maxresdefault.jpg"}
             alt="slide 3"
           />
+          <CCarouselCaption className="d-none d-md-block">
+            <h5>Third slide label</h5>
+            <p>Some representative placeholder content for the first slide.</p>
+          </CCarouselCaption>
         </CCarouselItem>
       </CCarousel>
     </div>
