f92f87cbe8d33805f076525f0466e13b739bd7d6
beth <bethcassdy@gmail.com>
2023-05-08 16:35:56 -0400

    new soup

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
 import { Header } from "./Header";
 import { Home } from "./Home";
 import { Footer } from "./Footer";
-import { BrowserRouter } from "react-router-dom";
+// import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <BrowserRouter>
-        <Header />
-        <Home />
-        <Footer />
-      </BrowserRouter>
+      <Header />
+      <Home />
+      <Footer />
     </div>
   );
 }
